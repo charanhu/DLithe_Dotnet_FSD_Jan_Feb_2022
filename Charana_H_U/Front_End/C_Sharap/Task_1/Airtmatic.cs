06f771b66c5cfd8321cb29e1def5e98c7443629f
charanhu <charanhumail@gmail.com>
2022-02-16 11:50:10 +0530

    Swap two without temp

---
+++ Charana_H_U/Front_End/C_Sharap/Task_1/Airtmatic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Program2
+{
+    class Airthmatic
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Airthmatic Operations");
+            int a = 50;
+            int b = 20;
+            Console.WriteLine("Addition: "+ (a + b));
+            Console.WriteLine("Substraction: " + (a - b));
+            Console.WriteLine("Multiplication: " + (a * b));
+            Console.WriteLine("Division: "+(a/b));
+            Console.WriteLine("Modulus: "+(a%b));
+        }
+    }
+}
---
+++ Charana_H_U/Front_End/C_Sharap/Task_1/Class2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Program3
+{
+    class Swap
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("a: ");
+            string a=Console.ReadLine();
+            int val1=Convert.ToInt32(a);  //20
+            Console.WriteLine("b: ");
+            string b=Console.ReadLine();
+            int val2=Convert.ToInt32(b);  //10
+            Console.WriteLine("Before Swap:");
+            Console.WriteLine("a: " + val1);
+            Console.WriteLine("b: " + val2);
+            val1 = val1 + val2;            //30=20+10
+            val2 = val1 - val2;           //20=30-10
+            val1 = val1 - val2;           //10=30-20
+            Console.WriteLine("After Swap:");
+            Console.WriteLine("a: " + val1);
+            Console.WriteLine("b: " + val2);
+        }
+
+
+    }
+}
