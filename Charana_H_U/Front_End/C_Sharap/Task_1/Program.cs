acb533092d6acc36991d815a0129b71590999e02
charanhu <charanhumail@gmail.com>
2022-02-16 10:44:29 +0530

    Get Employee details and print them

---
+++ Charana_H_U/Front_End/C_Sharap/Task_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task1
+{
+    class Employee
+    {
+        static void Main(string[] args)
+        {
+            //to get how many employees 
+            Console.WriteLine("Enter How many employess Data to store");
+            string val = Console.ReadLine();
+            int n = Convert.ToInt32(val);
+
+            //to store all employees id
+            String[] emp_id = new String[n];
+
+            //to store all employees name
+            String[] emp_name = new String[n];
+
+            //To read employee details
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter Employee "+(i+1)+" ID");
+                emp_id[i]= Console.ReadLine();
+                Console.WriteLine("Enter Employee  Name");
+                emp_name[i] = Console.ReadLine();
+            }
+            
+            //To dispaly the employee deatils
+            Console.WriteLine("Employee Details are");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("ID\t Name");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(emp_id[i]+"\t"+emp_name[i]);
+            }
+        }
+    }
+}
---
+++ Charana_H_U/Front_End/Task_18_DLithe_BC_NFS_T_Task22_jQuery/test.cs
@@ -0,0 +1,12 @@
+using System;
+
+// Hello World! program
+namespace HelloWorld
+{
+    class test {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
