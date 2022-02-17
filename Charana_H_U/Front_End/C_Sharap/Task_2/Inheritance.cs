9f13cb8074ea88054f8b124fb73ab1be8532331f
charanhu <charanhumail@gmail.com>
2022-02-17 10:28:28 +0530

    Task_13 first commit

--- Charana_H_U/Front_End/C_Sharap/Task_2/Inheritance.cs
+++ Charana_H_U/Front_End/C_Sharap/Task_2/Inheritance.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Inheritance
 
-namespace Task_2
+using System;
+
+namespace Inheritance
 {
-    internal class Inheritance
+
+
+    class School
     {
+        protected int s_id;
+        protected string s_name;
+
+        public void student_info(int s_id, string s_name)
+        {
+            this.s_id = s_id;
+            this.s_name = s_name;
+            Console.WriteLine("Student ID: " + s_id);
+            Console.WriteLine("Student NAME: " + s_name);
+        }
     }
-}
+
+    class Student : School
+    {
+        public StudentDetails()
+        {
+            Console.WriteLine("Student Details are");
+        }
+    }
+
+    class Main_Class
+    {
+        static void Main(String[] args)
+        {
+
+        }
+    }
+
+ }
---
+++ Charana_H_U/Front_End/C_Sharap/Task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Airthmatic
+{
+    class Addition
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello!, Welcome to C#");
+        }
+    }
+}
