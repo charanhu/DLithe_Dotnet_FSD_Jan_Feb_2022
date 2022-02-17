ae1dfeda1ab2ba544cd7c991c9152b15a0a3e55c
charanhu <charanhumail@gmail.com>
2022-02-17 11:07:01 +0530

    Task_2 Inheritance

--- Charana_H_U/Front_End/C_Sharap/Task_2/Inheritance.cs
+++ Charana_H_U/Front_End/C_Sharap/Task_2/Inheritance.cs
@@ -15,16 +15,25 @@ namespace Inheritance
         {
             this.s_id = s_id;
             this.s_name = s_name;
-            Console.WriteLine("Student ID: " + s_id);
-            Console.WriteLine("Student NAME: " + s_name);
+            Console.WriteLine("Base Student ID: " + s_id);
+            Console.WriteLine("Base Student NAME: " + s_name);
         }
     }
 
     class Student : School
     {
-        public StudentDetails()
+        //School sc=new School();
+        //sc.s_id = 1;
+        //sc.s_name = "charan";
+        public void StudentDetails()//parameters can also be possible here
         {
             Console.WriteLine("Student Details are");
+            s_id = 12;
+            s_name = "Arista";
+            School sc=new School();
+            sc.student_info(s_id, s_name);
+            Console.WriteLine("Derived Student ID: " + s_id);
+            Console.WriteLine("Derived Student NAME: " + s_name);
         }
     }
 
@@ -32,7 +41,8 @@ namespace Inheritance
     {
         static void Main(String[] args)
         {
-
+            Student student = new Student();
+            student.StudentDetails();
         }
     }
 
