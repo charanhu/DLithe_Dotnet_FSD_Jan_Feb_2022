d26742f4219109e423269e33e2545bccea84c5ec
charanhu <charanhumail@gmail.com>
2022-02-19 12:52:11 +0530

    Task_4 Debugging Challenge

---
+++ Charana_H_U/Front_End/C_Sharap/Task_4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int SNO, MARK1, MARK2, MARK3, AVERAGE;
+            string SNAME;
+            Console.WriteLine("ENTER THE STUDENT DETAILS");
+            Console.WriteLine("ENTER THE STUDENT NO");
+            SNO = int.Parse(Console.ReadLine());
+            Console.WriteLine("ENTER THE STUDENT NAME");
+            SNAME = Console.ReadLine();
+            Console.WriteLine("ENTER THE MARKS OF 3 SUBJECTS");
+            MARK1 = int.Parse(Console.ReadLine());
+            MARK2 = int.Parse(Console.ReadLine());
+            MARK3 = int.Parse(Console.ReadLine());
+            int TOTAL = MARK1 + MARK2 + MARK3;
+            AVERAGE = TOTAL / 3;
+            Console.WriteLine("\n\n\nTHE STUDENT DETAILS ARE GIVEN BELOW :");
+            Console.WriteLine("THE STUDENT NO IS: " + SNO);
+            Console.WriteLine("THE STUUDENT NAME IS: " + SNAME);
+            Console.WriteLine("TOTAL MARKS IS : " + TOTAL);
+            Console.WriteLine("AVEARGE MAARK IS: " + AVERAGE);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Charana_H_U/Front_End/C_Sharap/Task_4/Program2.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace PropertyDemo
+{
+    class Example
+    {
+        private int _empid, _eage;
+        private string _ename, _eaddress;
+        public int empid
+        {
+            set
+            {
+                empid = value;
+            }
+            get
+            {
+                return _empid;
+            }
+        }
+        public int eage
+        {
+            set
+            {
+                _eage = value;
+            }
+            get
+            {
+                return _eage;
+            }
+        }
+        public string ename
+        {
+            set
+            {
+                _ename = value;
+            }
+            get
+            {
+                return _ename;
+            }
+        }
+        public string eaddress
+        {
+            set
+            {
+                _eaddress = value;
+            }
+            get
+            {
+                return _eaddress;
+            }
+        }
+        static void Main()
+        {
+            Example example = new Example();
+            example._empid = 1;
+            example._eage = 2;
+            example._ename = "a";
+            example._eaddress = "b";
+            Console.WriteLine(example.empid);
+            Console.WriteLine(example.eage); 
+            Console.WriteLine(example.eaddress);
+            Console.WriteLine(example.ename);
+        }
+    }
+}
