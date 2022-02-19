38655fd11408f2a8fc8d24d2cbac8bf3a6baf76d
charanhu <charanhumail@gmail.com>
2022-02-19 13:27:10 +0530

    Task_4 Program 2

--- Charana_H_U/Front_End/C_Sharap/Task_4/Program2.cs
+++ Charana_H_U/Front_End/C_Sharap/Task_4/Program2.cs
@@ -1,66 +1,66 @@
-﻿using System;
+﻿//using System;
 
-namespace PropertyDemo
-{
-    class Example
-    {
-        private int _empid, _eage;
-        private string _ename, _eaddress;
-        public int empid
-        {
-            set
-            {
-                _empid = value;
-            }
-            get
-            {
-                return _empid;
-            }
-        }
-        public int eage
-        {
-            set
-            {
-                _eage = value;
-            }
-            get
-            {
-                return _eage;
-            }
-        }
-        public string ename
-        {
-            set
-            {
-                _ename = value;
-            }
-            get
-            {
-                return _ename;
-            }
-        }
-        public string eaddress
-        {
-            set
-            {
-                _eaddress = value;
-            }                                     // get method
-            get
-            {
-                return _eaddress;
-            }
-        }
-        static void Main()                           // Main method
-        {
-            Example example = new Example();                
-            example._empid = 101;
-            example._eage = 22;
-            example._ename = "Charan";
-            example._eaddress = "Shivamogga";
-            Console.WriteLine("EMP ID: "+example.empid);
-            Console.WriteLine("EMP AGE: "+example.eage);
-            Console.WriteLine("EMP NAME: "+example.ename);
-            Console.WriteLine("EMP ADDRESS: "+example.eaddress); 
-        }
-    }
-}
+//namespace PropertyDemo
+//{
+//    class Example2
+//    {
+//        private int _empid, _eage;
+//        private string _ename, _eaddress;
+//        public int empid
+//        {
+//            set
+//            {
+//                _empid = value;              // _empid
+//            }
+//            get
+//            {
+//                return _empid;
+//            }
+//        }
+//        public int eage
+//        {
+//            set
+//            {
+//                _eage = value;
+//            }
+//            get
+//            {
+//                return _eage;
+//            }
+//        }
+//        public string ename
+//        {
+//            set
+//            {
+//                _ename = value;
+//            }
+//            get
+//            {
+//                return _ename;
+//            }
+//        }
+//        public string eaddress
+//        {
+//            set
+//            {
+//                _eaddress = value;
+//            }                                     // get method
+//            get
+//            {
+//                return _eaddress;
+//            }
+//        }
+//        static void Main()                           // Main method
+//        {
+//            Example example = new Example();                
+//            example._empid = 101;
+//            example._eage = 22;
+//            example._ename = "Charan";
+//            example._eaddress = "Shivamogga";
+//            Console.WriteLine("EMP ID: "+example.empid);
+//            Console.WriteLine("EMP AGE: "+example.eage);
+//            Console.WriteLine("EMP NAME: "+example.ename);
+//            Console.WriteLine("EMP ADDRESS: "+example.eaddress); 
+//        }
+//    }
+//}
---
+++ Charana_H_U/Front_End/C_Sharap/Task_4/Program3.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace PropertyDemo
+{
+    class Example
+    {
+        public int _empid, _eage;
+        public string _ename, _eaddress;
+        public int empid
+        {
+            set
+            {
+                _empid = value;
+            }
+            get
+            {
+                return _empid;
+            }
+        }
+        public int eage
+        {
+            set
+            {
+                _eage = value;
+            }
+            get
+            {
+                return _eage;
+            }
+        }
+        public string ename
+        {
+            set
+            {
+                _ename = value;
+            }
+            get
+            {
+                return _ename;
+            }
+        }
+        public string eaddress
+        {
+            set
+            {
+                _eaddress = value;
+            }
+            get
+            {
+                return _eaddress;
+            }
+        }
+    }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Example obj = new Example();
+                obj._empid = 101;
+                obj._ename = "pranaya";
+                obj._eage = 27;
+                obj._eaddress = "bbsr";
+                Console.WriteLine("Employee details are:");
+                Console.WriteLine("employee id:" + obj.empid);
+                Console.WriteLine("employee name:" + obj.ename);
+                Console.WriteLine("employee age:" + obj.eage);
+                Console.WriteLine("employee address:" + obj.eaddress);
+                Console.ReadKey();
+            }
+        }
+    }
