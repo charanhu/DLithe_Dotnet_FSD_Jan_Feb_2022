9ed8db3af13822416a8d404bc3a25851879f8d58
charanhu <charanhumail@gmail.com>
2022-02-21 12:24:43 +0530

    Task_6 String

---
+++ Charana_H_U/Front_End/C_Sharap/Task_6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace String_Demo
+{
+    class Program1
+    {
+        static void Main()
+        {
+            // Strings 
+            string var1 = "Hello!, I'm Charan";
+            Console.WriteLine(var1);
+
+            // string length
+            string var2 = "This is Bootcamp";
+            Console.WriteLine("String Length: " + var2.Length);
+
+            // string concatenation
+            Console.WriteLine(var1 + var2);
+
+            // string interpolation
+            string var3 = "String1";
+            string var4 = "String2";
+            string var5 = $"{var3} and {var4}";
+            Console.WriteLine(var5);
+            Console.WriteLine();
+
+            // Access string
+            string var6 = "This is Welcome Meassage";
+            Console.WriteLine("String Access: " + var6[5]);
+
+            // IndexOf()
+            string var7 = "This is Welcome Meassage";
+            Console.WriteLine("First Index: " + var7.IndexOf("M"));
+
+            // LastIndexOf
+            string var8 = "This is Welcome Meassage";
+            Console.WriteLine("Last Index: "+var8.LastIndexOf("e"));
+
+        }
+    }
+}
