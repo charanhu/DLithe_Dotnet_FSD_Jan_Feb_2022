84325b500f59db907b1e89e7751a197509073581
charanhu <charanhumail@gmail.com>
2022-02-21 17:22:15 +0530

    Task_6 Partial_Clases

---
+++ Charana_H_U/Front_End/C_Sharap/Task_6/Partial_Class_1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EmployeeInfo
+{
+    class PartialClass1
+    {
+    }
+    public partial class EmployeeDetails
+    {
+        private int empID;
+        private string empName;
+        public EmployeeDetails(int empID, string empName)
+        {
+            this.empID = empID;
+            this.empName = empName;
+        }
+    }
+    public partial class Employee
+    {
+        public partial void InitializeEmployee();
+        public void Emp1() { }
+        public void Emp2() { }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeDetails ed = new EmployeeDetails(101, "Charan");
+            ed.PrintEmployeeInfo();
+            Employee emp = new Employee();
+            emp.InitializeEmployee();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Charana_H_U/Front_End/C_Sharap/Task_6/Partial_Class_2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeInfo
+{
+    class PartialClass2
+    {
+    }
+    public partial class Employee
+    {
+        public void Emphoyees() { }
+        public partial void InitializeEmployee()
+        {
+            string str = "Employee Details";
+            Console.WriteLine(str);
+        }
+    }
+    public partial class EmployeeDetails
+    {
+        public void PrintEmployeeInfo()
+        {
+            Console.WriteLine("Employee ID:" + empID);
+            Console.WriteLine("Employee Name:" + empName);
+        }
+    }
+}
