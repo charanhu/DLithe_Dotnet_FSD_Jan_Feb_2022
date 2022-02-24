c889cf8af4d74f146d53529a694ddd91028e8377
charanhu <charanhumail@gmail.com>
2022-02-24 10:47:52 +0530

    Angular Insurence Use Case

---
+++ Charana_H_U/Front_End/C_Sharap/Task_8/Array_List.cs
@@ -0,0 +1,29 @@
+﻿// C# Collections
+
+using System;
+using System.Collections;
+
+
+namespace ArrayListExample
+{
+    class ArrayListDemo
+    {
+        static void Main()
+        {
+            var data = new ArrayList();
+            Console.WriteLine("ArrayList")
+            data.Add(1);
+            data.Add(2);   
+            data.Add(3);
+            data.Add(4);
+            data.Add(55.12);
+            data.Add(45);
+            data.Remove(3);
+            foreach (var item in data)
+            {
+                Console.WriteLine(item);
+                Console.ReadLine(); 
+            }
+        }
+    }
+}
