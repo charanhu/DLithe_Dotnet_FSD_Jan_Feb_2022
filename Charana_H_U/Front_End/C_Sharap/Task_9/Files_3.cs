c5c111d4ac665cd1230f2fea1bdc453e9051043f
charanhu <charanhumail@gmail.com>
2022-02-25 17:35:37 +0530

    Task_9 Files

---
+++ Charana_H_U/Front_End/C_Sharap/Task_9/Files_3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+namespace File_
+{
+    class File_3
+    {
+        public static void Main()
+        {
+            string path = @"c:\users\charan\documents\MyTest1.txt";
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("Hello");
+                    sw.WriteLine("And");
+                    sw.WriteLine("Welcome");
+                    sw.WriteLine("Iam_Charan");
+                }
+            }
+
+            // Open the file to read from.
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+    }
+}
